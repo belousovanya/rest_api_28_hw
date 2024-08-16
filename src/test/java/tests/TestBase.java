e91a88088be398d4de21fd7bf0464cc423acd100
Anna Belousova <belusovanna@gmail.com>
2024-08-16 11:46:42 +0500

    added basePath

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,14 +5,15 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.BeforeAll;
 
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.filters;
+import static io.restassured.RestAssured.*;
 
 public class TestBase {
     @BeforeAll
     public static void setupTests() {
         filters(new RequestLoggingFilter(LogDetail.BODY), new ResponseLoggingFilter(LogDetail.BODY),
                 new ResponseLoggingFilter(LogDetail.STATUS));
-        baseURI = "https://reqres.in/api/";
+
+        baseURI = "https://reqres.in/";
+        basePath = "/api";
     }
 }
