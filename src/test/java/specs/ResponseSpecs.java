87994734944fc7a6517e5db01a23444e15ea7000
Anna Belousova <belusovanna@gmail.com>
2024-08-24 20:46:54 +0500

    added status codes to the variable name + added logging

--- src/test/java/specs/ResponseSpecs.java
+++ src/test/java/specs/ResponseSpecs.java
@@ -3,19 +3,23 @@ package specs;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.ResponseSpecification;
 
+import static io.restassured.filter.log.LogDetail.ALL;
 import static org.apache.http.HttpStatus.*;
 
 public class ResponseSpecs {
 
-    public static ResponseSpecification successResponseSpec = new ResponseSpecBuilder()
+    public static ResponseSpecification successResponseSpec200 = new ResponseSpecBuilder()
             .expectStatusCode(SC_OK)
+            .log(ALL)
             .build();
 
-    public static ResponseSpecification createdResponseSpec = new ResponseSpecBuilder()
+    public static ResponseSpecification createdResponseSpec201 = new ResponseSpecBuilder()
             .expectStatusCode(SC_CREATED)
+            .log(ALL)
             .build();
 
-    public static ResponseSpecification badRequestResponseSpec = new ResponseSpecBuilder()
+    public static ResponseSpecification badRequestResponseSpec400 = new ResponseSpecBuilder()
             .expectStatusCode(SC_BAD_REQUEST)
+            .log(ALL)
             .build();
 }
